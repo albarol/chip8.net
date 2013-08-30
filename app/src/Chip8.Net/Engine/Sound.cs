49714031bc11a21a6005f7b3b2c820fc2b1abf76
fakeezz <fakeezz@gmail.com>
2013-08-30 09:24:13 -0300

    changes assemblies

--- app/src/Chip8.Net/Engine/Sound.cs
+++ app/src/Chip8.Net/Engine/Sound.cs
@@ -1,20 +1,17 @@
 ﻿namespace Chip8.Net.Engine
 {
     using System.IO;
-
-    using Microsoft.VisualBasic;
-    using Microsoft.VisualBasic.Devices;
+	using System.Media;
 
     public class Sound
     {
-        private Audio audio;
+        private SoundPlayer audio;
         private byte[] bufferAudio;
         
         public Sound()
         {
             this.Enabled = true;
-            this.audio = new Audio();
-            this.bufferAudio = this.LoadAudio();
+            this.LoadAudio();
         }
 
         public int Timer { get; set; }
@@ -22,11 +19,7 @@
 
         public void Beep()
         {
-            
-            using (var beepAudio = new MemoryStream(this.bufferAudio))
-            {
-                this.audio.Play(beepAudio, AudioPlayMode.Background);    
-            }
+			this.audio.Play();
         }
 
         public void Update()
@@ -50,7 +43,10 @@
                 buffer = new byte[(int)reader.BaseStream.Length];
                 reader.Read(buffer, 0, buffer.Length);
             }
-            return buffer;
+			using (var beepAudio = new MemoryStream(buffer))
+            {
+				this.audio = new SoundPlayer(beepAudio);
+            }
         }
     }
 }
